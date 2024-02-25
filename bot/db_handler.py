019e28e48ca0e6c1f11f146accbf43b280635aac
gwendolynkwong <gwen.kwong1@gmail.com>
2024-02-25 00:07:54 -0500

    Added functions in db_handler.py.

--- bot/db_handler.py
+++ bot/db_handler.py
@@ -1,16 +1,58 @@
 from pymongo.mongo_client import MongoClient
 
-def get_chat_history(client: MongoClient, user: str) -> list:
+
+"""Return the collection containing chat history. This is a helper
+function.
+"""
+def find_db(client: MongoClient, user: str) -> dict:
     query = {"user": user}
-
     db = client["user_data"]
-    col = db["chat_history"]
-    doc = col.find_one(query)
+    return db["chat_history"]
 
+
+"""Return true iff this user already has a document in the chat history
+collection.
+"""
+def find_chat_history(client: MongoClient, user: str) -> bool:
+    col = find_db(client, user)
+    return col.find_one(query) != None
+
+
+"""Create a new document for this user in the chat history collection.
+Do nothing and return false iff this user already has a document.
+"""
+def init_chat_history(client: MongoClient, user: str) -> bool:
+    if find_chat_history(client, user) == True:
+        return False
+
+    col = find_db(client, user)
+    new_doc = {"user": user, "chat_history": []}
+    col.insert_one(new_doc)
+
+    return True
+
+
+"""Return the list containing the chat history of this user. Return
+an empty list iff this user does not have a document in the chat
+history collection.
+"""
+def get_chat_history(client: MongoClient, user: str) -> list:
+    if find_chat_history(client, user) == False:
+        return []
+
+    col = find_db(client, user)
+    doc = col.find_one(query)
     return doc["chat_history"]
 
 
-def record_chat_history(client: MongoClient, user: str, chat_history: list):
+"""Add to the chat history of this user. Return false iff this user
+does not have a document in the chat history collection.
+"""
+def record_chat_history(client: MongoClient, user: str, chat_history: \
+                        list) -> bool:
+    if find_chat_history(client, user) == True:
+        return False
+    
     query = {"user": user}
 
     db = client["user_data"]
@@ -21,3 +63,22 @@ def record_chat_history(client: MongoClient, user: str, chat_history: list):
     new_history.extend(chat_history)
 
     col.update_one(query, {"chat_history": new_history})
+
+    return True
+
+
+"""Clear the chat history of this user. Return false iff this user
+does not have a document in the chat history collection.
+"""
+def clear_chat_history(client: MongoClient, user: str):
+    if find_chat_history(client, user) == True:
+        return False
+
+    query = {"user": user}
+
+    db = client["user_data"]
+    col = db["chat_history"]
+
+    col.update_one(query, {"chat_history": []})
+
+    return True
