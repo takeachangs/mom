524117922679b832a04d8d9e0e8d65e3d075f58d
SeongminChang <seongmin.chang@mail.utoronto.ca>
2024-02-24 21:01:31 -0500

    Delete cohere_handler.py file

---
+++ bot/cohere_chat.py
@@ -0,0 +1,28 @@
+import cohere
+
+class CohereChat: 
+    def __init__(self, temperature, user):
+        """
+        Initialize Cohere client with API key
+        """
+        self.co = cohere.Client("6yJnofp1XcVL54HTVCh19zBlXnGLuDm9GdAqyQJi")
+        self.model = "command"
+        self.temperature = temperature
+        self.chat_history = []
+        self.user = user
+
+    def get_response(self, message, documents =[]):
+        """
+        Get response from Cohere API
+        """
+        response = self.co.chat(
+            chat_history=self.chat_history,
+            message=message,
+            temperature=self.temperature,
+            documents=documents
+        )
+        user_message = {"user_name": self.user, "text": message}
+        bot_message = {"user_name": "CHATBOT", "text": response.text}
+        
+        self.chat_history.append(user_message)
+        self.chat_history.append(bot_message)
